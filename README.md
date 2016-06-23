91a6b0678b1716c264b7079f6f2016e7555c8c6e
Marceli.no <me@marceli.no>
2016-06-23 11:16:17 -0400

    chore: Add continuous integration with coverage reporting
    Setting up Travis-CI and Coveralls to automate the process
    of verifying that changes are stable and to give users a
    better sense of confidence in the library. Also included
    the Travis/Coveralls shields/badges in the README.
    
    checkpoint

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
 # N.U.T.R.A — Node Unit Test Runner Auxiliary
+[![Coverage Status](https://coveralls.io/repos/github/m-a-r-c-e-l-i-n-o/nutra/badge.svg?branch=master)](https://coveralls.io/github/m-a-r-c-e-l-i-n-o/nutra?branch=master)
+[![Build Status](https://travis-ci.org/m-a-r-c-e-l-i-n-o/nutra.svg?branch=master)](https://travis-ci.org/m-a-r-c-e-l-i-n-o/nutra)
+
+
 The "nutra" module is a simple, extendable unit test runner for node.
 
 ## Quickstart
--- nutra.config.js
+++ nutra.config.js
@@ -4,10 +4,17 @@ module.exports = function( config ) {
         files: ['test/specs/**/*.js', 'src/**/*.js'],
         preprocessors: {
             'test/specs/**/*.js': ['nutra-babel'],
-            'src/**/*.js': ['nutra-babel']
+            'src/**/*.js': ['nutra-babel', 'nutra-coverage']
         },
+        reporters: ['nutra-coverage'],
         babelOptions: {
             configFile: './.babelrc'
+        },
+        coverageOptions: {
+            dir : './coverage/',
+            reporters: [
+                { type: 'lcovonly', subdir: '.', file: 'lcov.info' }
+            ]
         }
     })
 }
